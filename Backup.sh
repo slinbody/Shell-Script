e35c3d69d31892c8734a63e234f5f87057a57f84
slinbody <slinbody@gmail.com>
2014-11-17 17:06:20 +0800

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -13,3 +13,8 @@ src=/media/media1/officePublicPartition/
 echo `date +%Y.%m.%d`" "`date +%H:%M:%S`" backup 192.168.1.105 Starting !!"
 /usr/local/bin/rsync -az -e 'ssh -p 22 -o StrictHostKeyChecking=no' root@192.168.1.105:$src $dst
 echo `date +%Y.%m.%d`" "`date +%H:%M:%S`" backup 192.168.1.105 Finished !!!"
+
+#don't backup logs directory
+rsync -aR -v  -e 'ssh -p 22' --exclude='logs' --rsync-path='sudo rsync' ubuntu@54.2.11.1:/mnt $dst/urcosme_nginx/
+
+

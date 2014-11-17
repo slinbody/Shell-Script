b77bdc411e1d6de1017f79a04b730fe9b548b053
slinbody <slinbody@gmail.com>
2014-11-17 16:39:42 +0800

    Update AWS_Stop_Instance.sh

--- AWS_Stop_Instance.sh
+++ AWS_Stop_Instance.sh
@@ -4,8 +4,8 @@
 #=============================================
 
 echo `date +%Y.%m.%d`" "`date +%H:%M:%S`
-/usr/local/bin/aws ec2 stop-instances --instance-ids i-3686d661 #redmine.urcosme.com
+/usr/local/bin/aws ec2 stop-instances --instance-ids i-xxxxd661 #redmine.urcosme.com
 echo `date +%Y.%m.%d`" "`date +%H:%M:%S`
 #not Default Region
-/usr/local/bin/aws --region ap-northeast-1 ec2 stop-instances --instance-ids i-26dcab20 
+/usr/local/bin/aws --region ap-northeast-1 ec2 stop-instances --instance-ids i-xxxxab20 
 

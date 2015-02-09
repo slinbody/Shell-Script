b3f55b79ad526443cbc781421b8df19361827f68
slinbody <slinbody@gmail.com>
2015-02-09 15:06:20 +0800

    Update Generate_file.sh

--- Generate_file.sh
+++ Generate_file.sh
@@ -4,10 +4,13 @@ dir[1]="/tmp/test/b/"
 dir[2]="/tmp/test/c/"
 dir[3]="/tmp/test/d/"
 dir[4]="/tmp/test/e/"
-for i in {0..4}
+while :
 do
-name=${dir[$i]}$(cat /dev/urandom | tr -dc '[:alnum:]' | fold -w 32 | head -n 1)
-echo $name
-touch $name
-dd if=/dev/zero of=$name bs=512 count=200 > /dev/null
+  for i in {0..4}
+  do
+    name=${dir[$i]}$(cat /dev/urandom | tr -dc '[:alnum:]' | fold -w 32 | head -n 1)
+    echo $name
+    touch $name
+    dd if=/dev/zero of=$name bs=512 count=200 > /dev/null
+  done
 done

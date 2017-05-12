9fbc94b5ba4735ade6b20650b594a68112fbfef1
slinbody <slinbody@gmail.com>
2017-05-12 09:21:57 +0800

    Create check_amazon_file.sh

---
+++ check_amazon_file.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# check files that uploaded to amazon cloud drive by acd_cli
+
+acd_cli sync
+
+amazon_dir='/2017/'$1
+
+file=`acd_cli ls $amazon_dir | awk '{print $3}'`
+for i in $file
+do
+    echo -n $i": "
+    acd_cli metadata $amazon_dir"/"$i | sed -n 's/ *"size": \([0-9][0-9]*\),/\1/p'
+done
+
+local_dir='/mine/Amazon/'$1
+
+ls -la $local_dir | awk 'NR>1{if ($9 != "." && $9 != "..") {print $9": "$5}}'

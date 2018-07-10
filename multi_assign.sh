6fbc348ec0864dd527eacdd24f52c0246c86105c
slinbody <slinbody@gmail.com>
2018-07-10 15:14:55 +0800

    Update multi_assign.sh

--- multi_assign.sh
+++ multi_assign.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # This is multi variable assignment example
+
+# way 1.
 while read a b c
 do
     echo $a,$b,$c
 done << EOF
 $(awk '{print $1,$2,$3}' /tmp/123)
 EOF
+
+# way 2.
+while read a b c
+do
+    echo $a,$b,$c
+done < <(awk '{print $1,$2,$3}' /tmp/123)
+
 
 --------- /tmp/123 ---------
 a b c

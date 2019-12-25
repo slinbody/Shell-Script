2241501fa2fd7443af5c85d0d7d9191048528b32
slinbody <slinbody@gmail.com>
2019-12-25 19:23:26 +0800

    Update awk_ex1.sh

--- awk_ex1.sh
+++ awk_ex1.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-awk 'BEGIN {sum=10} {sum+=$2} END {print sum}' $1
+awk 'BEGIN {sum=10} $1 ~ /d/ {sum+=$2} END {print sum}' $1
 
 # input file content
-# a 1
-# b 2
-# c 5
-# d 5
+#a 1
+#b 2
+#c 4
+#d 5
+#d 6
+#d 8
+#d 7
+
